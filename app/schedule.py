94fe9570bcdc957ef68b867a79721d855448a8c9
philk <pk34663@gmail.com>
2017-10-02 21:46:28 +0100

    update schedule.py

--- app/schedule.py
+++ app/schedule.py
@@ -1,55 +1,79 @@
 import urllib2
 import BeautifulSoup
 import pprint
+import json
 import sys
 import re
 
 class bbc6schedule:
-    def __init__(self):
-        self._schedule = {}
+    def __init__(self, date):
+        self._schedule = {'date':date, 'programmes':{}}
+        self._baseurl = 'http://www.bbc.co.uk/6music/programmes/schedules/'
 
-    def parse_schedule(self,date):
-        baseurl = 'http://www.bbc.co.uk/6music/programmes/schedules/'
+    def get_html(self, url):
+        req = urllib2.Request(url)
+        response = urllib2.urlopen(req)
+        html = response.read()
+
+        return html
+
+    def parse_schedule(self):
         resourcere = re.compile('^.*typeof=\"RadioEpisode\" resource=\"(?P<resource>http://[^\"]+)\".*?name\">(?P<show>[^<]+).*')
 
-        req = urllib2.Request(baseurl + date)
-        response = urllib2.urlopen(req)
+        url = self._baseurl + self._schedule['date']
+        print 'DEBUG:parse_schedule %s' % (url)
+        html = self.get_html(url)
 
-        html = response.read()
         soup = BeautifulSoup.BeautifulSoup(html)
 
         shows = []
         for tag in soup.findAll(resource=True,typeof="RadioEpisode"):
             match =  resourcere.match(str(tag))
             if match:
+                self._schedule['programmes'][match.group('show')] = {
+                    'url':match.group('resource'),
+                    'tracks':[]}
                 shows.append((match.group('resource'),match.group('show')))
 
         return shows
 
-    def parse_programme(self,programme):
+    def parse_programme(self,programmeurl,programme):
         strbuffer = ""
         try:
-            req = urllib2.Request(programme + '/segments.inc')
-            response = urllib2.urlopen(req)
-            html = response.read()
+            url = programmeurl + '/segments.inc'
+            html = self.get_html(url)
+
             soup = BeautifulSoup.BeautifulSoup(html)
             #s = soup.findAll('span','artist')
             s = soup.findAll('div')
             for tag in s:
                 for attr in tag.attrs:
                     if 'data-title' in attr:
+                        (artist,track) = attr[1].replace(' || ','||').split('||')
+                        self._schedule['programmes'][programme]['tracks'].append((artist,track))
                         strbuffer += attr[1] + "\n"
-        except:
-            print "can't load programme ",programme
+        except Exception as e:
+            print 'exception %s\n', e
 
-        return strbuffer
+    def get_json(self):
+        return json.dumps(self._schedule)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print "%s Usage: %s" % (sys.argv[0], "date <YYYY/MM/DD>")
         sys.exit(1)
 
-    schedule = bbc6schedule() 
-    for programme in schedule.parse_schedule(sys.argv[1]):
-        print programme
-        print schedule.parse_programme(programme[0])
+    schedule = bbc6schedule(sys.argv[1]) 
+    for programme in schedule.parse_schedule():
+        schedule.parse_programme(programme[0],programme[1])
+    #for key in schedule._schedule['programmes']:
+    #    print key
+    #    for programme in schedule._schedule['programmes'][key]['tracks']:
+    #        print programme[0], programme[1]
+
+    data = json.loads(schedule.get_json())
+
+    for key in data['programmes']:
+        print key
+        for track in data['programmes'][key]['tracks']:
+            print 'artist: %s song: %s' % (track[0], track[1])
--- app/views.py
+++ app/views.py
@@ -1,12 +1,30 @@
 from app import app
 import schedule
 import time
+import json
 
 htmlheader = """
+<!DOCTYPE html>
 <html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+ <!-- Latest compiled and minified CSS -->
+ <link rel="stylesheet"
+ href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+ <!-- jQuery library -->
+ <script
+ src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+ <!-- Latest compiled JavaScript -->
+ <script
+ src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <body>
+        <div class="container">
 """
 htmlfooter = """
+        </div">
     </body>
 </html>
 """
@@ -16,13 +34,15 @@ htmlfooter = """
 def index():
     strbuffer = ""
     strbuffer += htmlheader
-    currentSchedule = schedule.bbc6schedule()
-    for programme in currentSchedule.parse_schedule(time.strftime("%Y/%m/%d")):
-        strbuffer += "<h2>" + programme[1] + "</h2>"
-        pinfo = currentSchedule.parse_programme(programme[0])
-        if pinfo != None:
-            tracks = pinfo.split('\n')
-            for track in tracks:
-                strbuffer += "<h3>"+ track + "</h3>"
+    currentSchedule = schedule.bbc6schedule('2017/10/02')
+    currentSchedule.parse_schedule()
+    for programme in currentSchedule.parse_schedule():
+        currentSchedule.parse_programme(programme[0],programme[1])
+    jsonstr = json.loads(currentSchedule.get_json())
+
+    for programme in jsonstr['programmes']:
+        strbuffer += "<h2>" + programme + "</h2>"
+        for track in jsonstr['programmes'][programme]['tracks']:
+            strbuffer += "<h3>%s, %s</h3>" % (track[0], track[1])
     strbuffer += htmlfooter
     return strbuffer
